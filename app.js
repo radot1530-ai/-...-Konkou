355ffe6bfccb98b353dd6be204e3274891a30ddd
radot1530-ai <radot1530@gmail.com>
2025-12-25 10:48:31 -0500

    Fix storageBucket URL in Firebase config

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
     authDomain: "kaylakay-cdf64.firebaseapp.com",
     databaseURL:"https://kaylakay-cdf64-default-rtdb.firebaseio.com/",
     projectId: "kaylakay-cdf64",
-    storageBucket: "kaylakay-cdf64.firebasestorage.app",
+    storageBucket:"kaylakay-cdf64.appspot.com",
     messagingSenderId: "663099511740",
     appId: "1:663099511740:web:aeb6bddccee9666ff791b9",
     measurementId: "G-JF9PNTTTG4"
