14bbf70bedbf7263847c71193acbc6a3f75420b7
radot1530-ai <radot1530@gmail.com>
2026-03-03 23:39:20 -0500

    Create Main activity.java

---
+++ app/src/main/java/com/ns4/quiz/Main activity.java
@@ -0,0 +1,22 @@
+package com.ns4.quiz;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/quiz.html");
+    }
+          }
