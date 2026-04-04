843529c1b9b988949937b48c231c3167f29c078d
radot1530-ai <radot1530@gmail.com>
2026-04-04 16:36:22 -0400

    Update MainActivity.java

--- app/src/main/java/com/ns4/quiz/MainActivity.java
+++ app/src/main/java/com/ns4/quiz/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         // 🔹 Ouvri URL ekstèn
         webView.setWebViewClient(new WebViewClient());
 
-        // 🔹 Chaje fichye lokal la (intro.html nan assets folder)
-        webView.loadUrl("file:///android_asset/intro.html");
+        // 🔹 Chaje fichye lokal la (.html nan assets folder)
+        webView.loadUrl("file:///android_asset/xxxxx.html");
     }
 
     // 🔙 Back button pou navigasyon WebView
